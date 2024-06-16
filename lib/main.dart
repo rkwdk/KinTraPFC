9ec6a2c7299aef979aa2d7755e7bba2a3e2a5599
zio <rkwdk2020@gmail.com>
2024-06-16 12:41:37 +0900

    ディレクトリ構造を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:nutri_meter/features/home/screen/home_screen.dart';
+import 'package:nutri_meter/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      // localizationsDelegates: [
-      //   GlobalWidgetsLocalizations.delegate,
-      //   GlobalMaterialLocalizations.delegate,
-      //   GlobalCupertinoLocalizations.delegate
-      // ],
-      localizationsDelegates: L10n.localizationsDelegates,
-      // supportedLocales: [
-      //   Locale('ja', 'JP'),
-      // ],
-      supportedLocales: L10n.supportedLocales,
-      home: HomeScreen(),
-    );
-  }
 }
