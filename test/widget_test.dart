76486aed86770fbbca3c2e6d0572e1257b62a5cd
zio <rkwdk2020@gmail.com>
2024-06-16 14:18:59 +0900

    rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:nutri_meter/main.dart';
+import 'package:nutri_meter/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
